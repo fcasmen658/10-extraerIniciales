07d3d1038fdc6f44fd9280d8750816c29ae9be77
Francisco M.C.M. <fcasmen658@gmail.com>
2024-07-17 12:21:29 +0200

    Comentarios y librerías

--- src/extraerIniciales.java
+++ src/extraerIniciales.java
@@ -1,20 +1,23 @@
+import org.iesalandalus.programacion.utilidades.Entrada;
 public class extraerIniciales {
     public static void main(String[] args) {
+// Variables
         String nombre;
-        nombre = "Francisco Miguel CASAS MÉNDEZ";
-        String getIniciales;
-        {
-            String iniciales = "";
-            for (int i = 0; i < nombre.length(); i++) {
-                if (i == 0 || nombre.charAt(i - 1) == ' ') {
-                    iniciales += nombre.charAt(i);
-                }
+        System.out.print("Introduzca su nombre completo: ");
+        nombre = Entrada.cadena();
+        System.out.println();
+// Iniciales
+        String iniciales = "";
+        for (int i = 0; i < nombre.length(); i++) {
+            if (i == 0 || nombre.charAt(i - 1) == ' ') {
+                iniciales += nombre.charAt(i);
             }
-
-            System.out.println((" ").repeat(10));
-            System.out.println("El nombre completo es: " + nombre);
-            System.out.println("Las iniciales del nombre son: " + iniciales);
-            System.out.println((" ").repeat(10));
         }
+// Salida por pantalla
+        System.out.println((" ").repeat(10));
+        System.out.print("El nombre completo es: " + nombre);
+        System.out.println();
+        System.out.print("Las iniciales del nombre son: " + iniciales);
+        System.out.println((" ").repeat(10));
     }
 }
